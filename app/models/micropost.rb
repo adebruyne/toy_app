28a91b6a5ddc07ceee1f56b4b47788077a20cb26
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-28 20:24:43 -0500

    adds a validator to content for microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,7 @@ class Micropost < ApplicationRecord
     # a micropost belongs to a user
     belongs_to :user
     # should limit the content of microposts to 140 characters
-    validates :content, length: { maximum: 140}
+    validates :content, length: { maximum: 140},
+                        presence: true
+
 end
